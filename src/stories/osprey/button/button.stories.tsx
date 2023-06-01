73e1d087a13e7064973666ab04b3fe1c8c710925
ericsison <ericsison.dev@gmail.com>
2023-06-01 17:26:13 +0800

    renamed storybook route from example to components

--- src/stories/osprey/button/button.stories.tsx
+++ src/stories/osprey/button/button.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Button as ButtonComponent } from "../../../components/button/view/Button";
 
 const meta = {
-  title: "Example/Button",
+  title: "Components/Button",
   component: ButtonComponent,
   parameters: {
     layout: "centered",
