067126d2f0066d45648d635cd8033a3482af7132
ericsison <ericsison.dev@gmail.com>
2023-06-01 17:23:36 +0800

    added hideNoControlsWarning in the preview config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -10,6 +10,7 @@ const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
+      hideNoControlsWarning: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
